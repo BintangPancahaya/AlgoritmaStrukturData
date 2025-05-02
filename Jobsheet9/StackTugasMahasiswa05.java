28f96cf6023fcbd9bae92d58f239df609ce1e49a
BintangPancahaya <bintang.panca007@gmail.com>
2025-05-02 14:42:07 +0700

    Jobsheet 9

---
+++ Jobsheet9/StackTugasMahasiswa05.java
@@ -0,0 +1,67 @@
+package Jobsheet9;
+
+public class StackTugasMahasiswa05 {
+    Mahasiswa05[] stack;
+    int size;
+    int top;
+
+    public StackTugasMahasiswa05(int size){
+        this.size = size;
+        stack = new Mahasiswa05[size];
+        top = -1;
+    }
+    
+    public boolean isFull(){
+        if(top == size - 1){
+            return true;
+        } else { return false; }
+    }
+
+    public boolean isEmpty(){
+        if(top == -1){
+            return true;
+        } else { return false; }
+    }
+
+    public void push(Mahasiswa05 mhs){
+        if(!isFull()){
+            top++;
+            stack[top] = mhs;
+        } else { System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi."); }
+    }
+
+    public Mahasiswa05 pop(){
+        if(!isEmpty()){
+            Mahasiswa05 m = stack[top];
+            top--;
+            return m;
+        } else {System.out.println("Stack kosong! Tidak ada tugas untuk dinilai."); return null; }
+    }
+
+    public Mahasiswa05 peek(){
+        if(!isEmpty()){
+            return stack[top];
+        } else { System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan."); return null; }
+    }
+
+    public void print(){
+        for(int i = 0; i <= top; i++){
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+
+    public String konversiDesimalKeBiner(int nilai){
+        StackKonversi05 stack = new StackKonversi05();
+        while(nilai > 0){
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai/2;
+        }
+        String biner = new String();
+        while(!stack.isEmpty()){
+            biner += stack.pop();
+        }
+        return biner;
+    }
+}
---
+++ Jobsheet9/Surat05Demo.java
@@ -0,0 +1,11 @@
+package Jobsheet9;
+
+import java.util.Scanner;
+
+public class Surat05Demo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
